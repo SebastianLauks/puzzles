21c1c66e86c64abb2dd43bdd01e759ec6a889020
Sebastian <sebastian.lauks13@gmail.com>
2021-05-08 09:51:59 +0200

    It is possible to drag and drop puzzles

--- app/src/main/java/lauks/sebastian/sm_p3/MainActivity.kt
+++ app/src/main/java/lauks/sebastian/sm_p3/MainActivity.kt
@@ -1,9 +1,15 @@
 package lauks.sebastian.sm_p3
 
+import android.content.ClipData
+import android.content.ClipDescription
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.DisplayMetrics
 import android.util.Log
+import android.view.DragEvent
+import android.view.View
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TableLayout
@@ -23,14 +29,43 @@ class MainActivity : AppCompatActivity() {
         mutableListOf<ImageView>()
     )
 
+    private lateinit var cutImage: List<List<Bitmap>>
+    private lateinit var cutImageShuffled: List<List<Bitmap>>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        cutImage = cutImage(BitmapFactory.decodeResource(resources, R.drawable.landscape_1))
+        cutImageShuffled = shuffleImage(cutImage)
         createBoardLayouts()
         createPatternLayouts()
     }
 
+
+    private fun shuffleImage(image: List<List<Bitmap>>): List<List<Bitmap>> {
+        var output = mutableListOf<MutableList<Bitmap>>(mutableListOf(), mutableListOf(), mutableListOf())
+
+        var list = mutableListOf<Bitmap>()
+
+        image.forEach {row ->
+            row.forEach {
+               list.add(it)
+            }
+        }
+
+        list.shuffle()
+
+        var listCounter = 0
+        for(i in 0..2){
+            for(j in 0..3){
+                output[i].add(list[listCounter++])
+            }
+        }
+        return output
+
+
+    }
 
     private fun createBoardLayouts() {
 
@@ -54,14 +89,38 @@ class MainActivity : AppCompatActivity() {
                 val imageView = ImageView(this).apply {
                     id = j
                     adjustViewBounds = true
-                    scaleType = ImageView.ScaleType.FIT_CENTER
+                    scaleType = ImageView.ScaleType.FIT_XY
                     layoutParams = TableRow.LayoutParams(
                         Integer.min(height, width) / 4,
                         Integer.min(height, width) / 6
                     )
                     setBackgroundResource(R.drawable.image_border)
                 }
+                imageView.setOnDragListener { v, event ->
+                    when(event.action){
+                        DragEvent.ACTION_DRAG_STARTED -> {
+                            true
+                        }
+                        DragEvent.ACTION_DROP -> {
+                            val item = event.clipData.getItemAt(0)
+                            val indexes = item.text.toString().toInt()
+                            val y = indexes % 10
+                            val x = (indexes - y) / 10
+                            if(cutImage[i][j].sameAs(cutImageShuffled[x][y])) {
+                                (v as ImageView).setImageBitmap(cutImage[i][j])
+                                pattern[x][y].setImageDrawable(null)
+                                pattern[x][y].setOnTouchListener { v, event ->
+                                    false
+                                }
+                                true
+                            }else false
+                        }
+                        else -> {
+                            false
+                        }
+                    }
 
+                }
                 //SET ON CLICK HERE
 //                imageView.setOnClickListener {  }
                 board[i].add(imageView)
@@ -100,16 +159,21 @@ class MainActivity : AppCompatActivity() {
                 val imageView = ImageView(this).apply {
                     id = j*10
                     adjustViewBounds = true
-                    scaleType = ImageView.ScaleType.FIT_CENTER
+                    scaleType = ImageView.ScaleType.FIT_XY
                     layoutParams = TableRow.LayoutParams(
                         Integer.min(height, width) / 4,
                         Integer.min(height, width) / 6
                     )
-                    setPadding(1,1,1,1)
 //                    setBackgroundColor(resources.getColor(R.color.colorPrimary))
+                    setImageBitmap(cutImageShuffled[i][j])
                     setBackgroundResource(R.drawable.image_border)
                 }
-
+                imageView.setOnTouchListener { v, event ->
+                    val item = ClipData.Item((i*10 + j).toString() as CharSequence)
+                    val dragData = ClipData((i*10 + j).toString() as CharSequence, arrayOf(ClipDescription.MIMETYPE_TEXT_PLAIN), item)
+                    val shadow = View.DragShadowBuilder(v)
+                    v.startDragAndDrop(dragData, shadow, null, 0)
+                }
                 //SET ON CLICK HERE
 //                imageView.setOnClickListener {  }
                 pattern[i].add(imageView)
@@ -124,6 +188,23 @@ class MainActivity : AppCompatActivity() {
                 )
             )
         }
+    }
+
+    private fun cutImage(img: Bitmap): List<List<Bitmap>> {
+        val displayMetrics = DisplayMetrics()
+        windowManager.defaultDisplay.getMetrics(displayMetrics)
+        val screenWidth = displayMetrics.widthPixels - 50
+        val screenHeight = displayMetrics.heightPixels - 10
+        val image = Bitmap.createScaledBitmap(img, screenWidth, screenHeight/2, false)
+        val output = mutableListOf<MutableList<Bitmap>>(mutableListOf(), mutableListOf(), mutableListOf())
+        val width = image.width
+        val height = image.height
+        for(i in 0..3){
+            for(j in 0..2){
+                output[j].add(Bitmap.createBitmap(image, (width * i)/4, (height * j)/3, width / 4 , height / 3))
+            }
+        }
+        return output
     }
 
 
